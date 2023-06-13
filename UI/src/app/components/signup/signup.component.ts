7dd221c92a6a0739a5c95ad7fd1b455f533d5cda
ElmedinaMorinaa <95186204+ElmedinaMorina@users.noreply.github.com>
2023-06-13 18:33:59 +0200

    UI for Login and Sign up created and some changes in backEnd

---
+++ UI/src/app/components/signup/signup.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import validateAllForm from 'src/app/helpers/validateform';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash';
+  signUpForm!: FormGroup;
+  message: string = 'message';
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+  hideShowPass() {
+    this.isText = !this.isText;
+
+    this.isText ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
+    this.isText ? (this.type = 'text') : (this.type = 'password');
+  }
+
+  onSignup() {
+    console.log('signup success!!!!');
+    this.router.navigate(['login']);
+    if (this.signUpForm.valid) {
+      this.auth.signUp(this.signUpForm.value).subscribe({
+        next: (res) => {
+          alert(res.message);
+          this.signUpForm.reset();
+          this.router.navigate(['login']);
+        },
+        error: (err) => alert(err?.error.message),
+      });
+    } else {
+      //logic for error
+      validateAllForm.validateAllFormFields(this.signUpForm);
+    }
+  }
+}
